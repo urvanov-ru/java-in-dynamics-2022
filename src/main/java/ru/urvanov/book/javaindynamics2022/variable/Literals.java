2a0ce41b0cedd62fc1c56ad09402bf56cb30f1a2
Fedor Urvanov <fedor@urvanov.ru>
2021-09-23 11:03:19 +0300

    примеры литералов

---
+++ src/main/java/ru/urvanov/book/javaindynamics2022/variable/Literals.java
@@ -0,0 +1,13 @@
+package ru.urvanov.book.javaindynamics2022.variable;
+
+public class Literals {
+    public static void main(String[] args) {
+        // Примеры литералов (после знака присваивания)
+        // true, 'f', 100000, 100, 10000 - это всё литералы.
+        boolean b = true;
+        char ch1 = 'f';
+        int i1 = 100000;
+        byte b1 = 100;
+        short sh1 = 10000;
+    }
+}
