b7bb8d31ea4ddae3d872927662b47597259d3cc9
Fedor Urvanov <fedor@urvanov.ru>
2021-09-24 11:21:32 +0300

    WideningPrimitiveConversion

---
+++ src/main/java/ru/urvanov/book/javaindynamics2022/operation/WideningPrimitiveConversion.java
@@ -0,0 +1,17 @@
+package ru.urvanov.book.javaindynamics2022.operation;
+
+/**
+ * Примеры расширяющего преобразования примитивов
+ */
+public class WideningPrimitiveConversion {
+    public static void main(String[] args) {
+        byte b1 = 100;
+        short sh1 = b1;  // Расширяющее преобразование byte->short
+        char ch1 = 100;
+        int i1 = sh1;    // Расширяющее преобразование short->int
+        int i2 = ch1;    // Расширяющее преобразование char->int
+        long l1 = i1;    // Расширяющее преобразование int->long
+        float f1 = l1;   // Расширяющее преобразование long->float
+        double d1 = f1;  // Расширяющее преобразование float->double
+    }
+}
