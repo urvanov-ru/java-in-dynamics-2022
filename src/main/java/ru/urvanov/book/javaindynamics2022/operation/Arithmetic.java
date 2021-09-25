ec4b02a152ed40cb01ae5529c58ebb909d60f6d3
Fedor Urvanov <fedor@urvanov.ru>
2021-09-25 19:24:17 +0300

    Arithmetic

---
+++ src/main/java/ru/urvanov/book/javaindynamics2022/operation/Arithmetic.java
@@ -0,0 +1,15 @@
+package ru.urvanov.book.javaindynamics2022.operation;
+
+/**
+ * Примеры арифметических операций.
+ */
+public class Arithmetic {
+    public static void main(String[] args) {
+        double x1 = 1.1 + 2.3;   // 3.4
+        double x2 = 1.1 - 0.1;   // 1.0
+        double x3 = 1.1 * 2 + 1; // 3.2
+        double x4 = 6 / 2.0;   // 3.0
+        int x5 = 12 + 3; // 15
+        int x6 = 13 % 5; // 3
+    }
+}
