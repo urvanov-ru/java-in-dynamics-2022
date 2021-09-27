73d8fa6968b0adbce5ec3207a9a4a2d64718e18e
Fedor Urvanov <fedor@urvanov.ru>
2021-09-27 19:12:38 +0300

    Block of code

---
+++ src/main/java/ru/urvanov/book/javaindynamics2022/block/Declaration.java
@@ -0,0 +1,10 @@
+package ru.urvanov.book.javaindynamics2022.block;
+
+/**
+ * Пример объявления переменной
+ */
+public class Declaration {
+    public static void main(String[] args) {
+        double myVal = 3.459;
+    }
+}
