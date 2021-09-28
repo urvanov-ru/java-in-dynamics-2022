623e7bac522a719704ff0eea99f07266ec9282b9
Fedor Urvanov <fedor@urvanov.ru>
2021-09-28 14:12:36 +0300

    Instanceof

--- src/main/java/ru/urvanov/book/javaindynamics2022/operation/InstanceOf.java
+++ src/main/java/ru/urvanov/book/javaindynamics2022/operation/InstanceOf.java
@@ -21,12 +21,14 @@ public class InstanceOf {
             System.out.println("NO");
         }
 
+
         // Переменная obj3 типа Object, но фактически содержит LocalDate
         Object obj3 = LocalDate.of(2021, 9, 28);
         if (obj3 instanceof LocalDate) {
             LocalDate myDate2 = (LocalDate) obj3;
             System.out.println("year=" + myDate2.getYear());
         }
+
         // pattern matching instance of
         if (obj3 instanceof LocalDate myDate2) {
             System.out.println("year=" + myDate2.getYear());
