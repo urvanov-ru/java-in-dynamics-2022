f47a93ea668515ec59b007ea2c1010f6e25c918d
Fedor Urvanov <fedor@urvanov.ru>
2021-09-28 17:12:18 +0300

    While

---
+++ src/main/java/ru/urvanov/book/javaindynamics2022/controlflow/WhileEndless.java
@@ -0,0 +1,13 @@
+package ru.urvanov.book.javaindynamics2022.controlflow;
+
+/**
+ * Пример бесконечного цикла while
+ */
+public class WhileEndless {
+    public static void main(String[] args) {
+        // Бесконечный цикл
+        while (true) {
+            // операторы
+        }
+    }
+}
