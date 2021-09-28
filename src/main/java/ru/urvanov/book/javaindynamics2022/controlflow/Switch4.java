2d06534fa32802d44e434ea8ca8354a49508f71c
Fedor Urvanov <fedor@urvanov.ru>
2021-09-28 14:01:56 +0300

    Switch

--- src/main/java/ru/urvanov/book/javaindynamics2022/controlflow/Switch4.java
+++ src/main/java/ru/urvanov/book/javaindynamics2022/controlflow/Switch4.java
@@ -1,15 +1,12 @@
 package ru.urvanov.book.javaindynamics2022.controlflow;
 
-public class Switch3 {
+public class Switch4 {
     public static void main(String[] args) {
         int mode = 1;
 
         switch (mode) {
             case 0:
                 System.out.println("mode 0");
-                break;
-            default:
-                System.out.println("mode default");
                 break;
             case 1:
                 System.out.println("mode 1");
@@ -18,6 +15,5 @@ public class Switch3 {
                 System.out.println("mode 2");
                 break;
         }
-
     }
 }
