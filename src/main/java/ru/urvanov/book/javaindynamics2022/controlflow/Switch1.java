6eb0bc2e500de9de3b252ca88a8685c12f3c77b0
Fedor Urvanov <fedor@urvanov.ru>
2021-09-28 13:42:25 +0300

    Switch

---
+++ src/main/java/ru/urvanov/book/javaindynamics2022/controlflow/Switch1.java
@@ -0,0 +1,24 @@
+package ru.urvanov.book.javaindynamics2022.controlflow;
+
+public class Switch1 {
+
+    public static void main(String[] args) {
+        int mode = 1;
+
+        switch (mode) {
+            case 0:
+                // operators for mode 0
+                break;
+            case 1:
+                // operators for mode 1
+                break;
+            case 2:
+                // operators for mode 2
+                break;
+            default:
+                // operators for other mode
+                break;
+        }
+
+    }
+}
