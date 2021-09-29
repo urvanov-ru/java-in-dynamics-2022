82fe5f23698cc8c4f1a9524ee002ac383f18017a
Fedor Urvanov <fedor@urvanov.ru>
2021-09-29 17:12:47 +0300

    Continue

---
+++ src/main/java/ru/urvanov/book/javaindynamics2022/controlflow/Continue.java
@@ -0,0 +1,13 @@
+package ru.urvanov.book.javaindynamics2022.controlflow;
+
+/**
+ * Пример с continue
+ */
+public class Continue {
+    public static void main(String[] args) {
+        for (int n = 0; n <= 10; n++) {
+            if (n % 2 == 0) continue;
+            System.out.println(n);
+        }
+    }
+}
