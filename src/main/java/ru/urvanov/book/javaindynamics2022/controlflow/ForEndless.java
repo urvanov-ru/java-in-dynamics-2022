714461c187797e69804964401bd43db2cdc12740
Fedor Urvanov <fedor@urvanov.ru>
2021-09-29 11:41:17 +0300

    For

---
+++ src/main/java/ru/urvanov/book/javaindynamics2022/controlflow/ForEndless.java
@@ -0,0 +1,11 @@
+package ru.urvanov.book.javaindynamics2022.controlflow;
+
+/**
+ * Пример бесконечного цикла For
+ */
+public class ForEndless {
+    public static void main(String[] args) {
+        // Бесконечный цикл
+        for (;;);
+    }
+}
