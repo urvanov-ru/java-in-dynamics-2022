60256555bbe88ee8ec3f7c98d1ade12026eb4985
Fedor Urvanov <fedor@urvanov.ru>
2021-10-02 14:45:32 +0300

    GoblinFields

---
+++ src/main/java/ru/urvanov/javaindynamics2022/classes/GoblinFields.java
@@ -0,0 +1,8 @@
+package ru.urvanov.javaindynamics2022.classes;
+
+public class GoblinFields {
+    private int money;
+    double health;
+    protected int diamonds = 10;
+    public String name;
+}
