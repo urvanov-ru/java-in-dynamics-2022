d2a8efabf1ecf55aa8173567440b85080bad7871
Fedor Urvanov <fedor@urvanov.ru>
2021-10-02 15:58:31 +0300

    GoblinMethods

--- src/main/java/ru/urvanov/javaindynamics2022/classes/GoblinMethods.java
+++ src/main/java/ru/urvanov/javaindynamics2022/classes/GoblinMethods.java
@@ -10,4 +10,44 @@ public class GoblinMethods {
         // some operators;
         return hitPoints;
     }
+
+    public int returnSix() {
+        return 6;
+    }
+
+    public double sum(double x1, double x2) {
+        return x1 + x2;
+    }
+
+    public void myVoidMethod1() {
+        // ...
+        if (hitPoints > 3) {
+            return;
+        }
+        //...
+    }
+
+    public void hit(Axe axe) {
+        // ... operators
+    }
+
+    public void hit(Flail flail) {
+        // ... operators
+    }
+
+    public void hit(Scimitar scimitar) {
+        // ... operators
+    }
+
+    public void hit(Torch torch) {
+        // ... operators
+    }
+
+    public void hit(Sword sword) {
+        // ... operators
+    }
+
+    public void hit(Sword sword, int comboCount) {
+        // ... operators
+    }
 }
