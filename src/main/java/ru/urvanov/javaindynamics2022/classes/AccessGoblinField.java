ec4b0a7219b6e6b36ac576d1fe00dc7e911e6242
Fedor Urvanov <fedor@urvanov.ru>
2021-10-04 14:05:35 +0300

    fix comment GoblinObj

--- src/main/java/ru/urvanov/javaindynamics2022/classes/AccessGoblinField.java
+++ src/main/java/ru/urvanov/javaindynamics2022/classes/AccessGoblinField.java
@@ -2,7 +2,7 @@ package ru.urvanov.javaindynamics2022.classes;
 
 public class AccessGoblinField {
     public static void main(String[] args) {
-        // Создаём объект GoblinObj
+        // Создаём объект GoblinFire
         GoblinFire goblinObj = new GoblinFire();
 
         // Обращение к полю ammo
