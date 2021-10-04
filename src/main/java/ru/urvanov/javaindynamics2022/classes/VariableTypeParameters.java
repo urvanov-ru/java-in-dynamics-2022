bab0f47c1995753eab3b73d60433deaf2e672acf
Fedor Urvanov <fedor@urvanov.ru>
2021-10-04 13:05:41 +0300

    VariableTypeParameters

---
+++ src/main/java/ru/urvanov/javaindynamics2022/classes/VariableTypeParameters.java
@@ -0,0 +1,22 @@
+package ru.urvanov.javaindynamics2022.classes;
+
+/**
+ * Пример метода с переменным числом параметров
+ */
+public class VariableTypeParameters {
+    public void sum(
+            int par1,
+            double par2,
+            String par3,
+            int... lastParameter) {
+        System.out.println("lastParameter[0] = " + lastParameter[0]); // 3
+        System.out.println("lastParameter[1] = " + lastParameter[1]); // 5
+        //...
+
+    }
+
+    public static void main(String[] args) {
+        VariableTypeParameters main = new VariableTypeParameters();
+        main.sum(100, 3.4, "par3", 3, 5, 6, 8, 9);
+    }
+}
