b1364dad102fb995c9d9481614e1f09c91ab5fe4
Fedor Urvanov <fedor@urvanov.ru>
2021-10-04 13:42:06 +0300

    Orc

---
+++ src/main/java/ru/urvanov/javaindynamics2022/classes/Orc.java
@@ -0,0 +1,17 @@
+package ru.urvanov.javaindynamics2022.classes;
+
+class Orc {
+    double health = 100.0;
+
+    public void someMethod1() {
+        double health = 200.0;
+        System.out.println(health); // 200.0
+        System.out.println(this.health); // 100.0
+    }
+
+    public void setHealth(double health) {
+        // присваиваем свойству класса
+        // переданное значение
+        this.health = health;
+    }
+}
