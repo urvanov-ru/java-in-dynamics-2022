d586715393de9f71d16f976727daf9802b77cecc
Fedor Urvanov <fedor@urvanov.ru>
2021-10-05 15:18:10 +0300

    IceElementalAnnotated

---
+++ src/main/java/ru/urvanov/javaindynamics2022/annotation/IceElementalAnnotated.java
@@ -0,0 +1,13 @@
+package ru.urvanov.javaindynamics2022.annotation;
+
+@Monster(
+        author="John Clark",
+        sprites = "Izabella Simpson",
+        sound = "Michael Lermontov",
+        code = "Pushkin A.",
+        createdAt = "2016-03-30",
+        description = "Ice elemental is a powerful creature from ice",
+        comments = {"lol", "gg", "Аффтар жжёт"}
+)
+class IceElementalAnnotated {
+}
