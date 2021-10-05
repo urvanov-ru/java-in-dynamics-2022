1377b35268e2c48f25f27ea504d3fa7cd633c2f6
Fedor Urvanov <fedor@urvanov.ru>
2021-10-05 15:12:57 +0300

    IceElemental

---
+++ src/main/java/ru/urvanov/javaindynamics2022/annotation/IceElemental.java
@@ -0,0 +1,13 @@
+package ru.urvanov.javaindynamics2022.annotation;
+
+public class IceElemental {
+    // author : John Clark
+    // sprites : Izabella Simpson
+    // sound : Michael Lermontov
+    // code : Pushkin A.
+    // createdAt : 2016-03-30
+    // description : Ice elemental is a powerful creature from ice.
+    // comments :
+
+    // ... code
+}
