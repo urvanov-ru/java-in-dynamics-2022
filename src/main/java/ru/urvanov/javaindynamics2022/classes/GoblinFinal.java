50bbe21ca6cfdfd9763874cd0160eff980ef494a
Fedor Urvanov <fedor@urvanov.ru>
2021-10-05 11:52:25 +0300

    GoblinFinal

---
+++ src/main/java/ru/urvanov/javaindynamics2022/classes/GoblinFinal.java
@@ -0,0 +1,27 @@
+package ru.urvanov.javaindynamics2022.classes;
+
+class GoblinFinal {
+    final String name;
+
+    public GoblinFinal (String name) {
+        this.name = name; // Инициализируем переменную final.
+    }
+
+    public void someMethod1(final String secondName) {
+        final String thirdName = "Third";
+
+        // Переменные с final менять после инициализации нельзя!
+        // this.name = secondName; //Нельзя! Ошибка компиляции.
+        // secondName = "3"; // Нельзя! Ошибка компиляции.
+        // thirdName = "4"; // Нельзя! Ошибка компиляции.
+    }
+
+
+    // Этот метод нельзя переопределять в потомках.
+    public final void myFinalMethod1() {
+    }
+
+    // Этот метод нельзя скрывать в потомках.
+    public static final void myFinalMethod2() {
+    }
+}
---
+++ src/main/java/ru/urvanov/javaindynamics2022/classes/GoblinFinalClass.java
@@ -0,0 +1,4 @@
+package ru.urvanov.javaindynamics2022.classes;
+
+final class GoblinFinalClass {
+}
