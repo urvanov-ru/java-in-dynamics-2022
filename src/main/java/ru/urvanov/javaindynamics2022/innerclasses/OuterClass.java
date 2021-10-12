7ffaeefd7c57ae4b542750e92531284417be2425
Fedor Urvanov <fedor@urvanov.ru>
2021-10-12 15:04:05 +0300

    OuterClass with InnerClass

--- src/main/java/ru/urvanov/javaindynamics2022/innerclasses/OuterClass.java
+++ src/main/java/ru/urvanov/javaindynamics2022/innerclasses/OuterClass.java
@@ -3,5 +3,17 @@ package ru.urvanov.javaindynamics2022.innerclasses;
 /**
  * Примеры вложенных классов
  */
-public class OuterClass {
+class OuterClass {
+
+    // Вложенный класс
+    class NestedClass {
+    }
+
+    // Статический вложенный класс
+    static class StaticNestedClass {
+    }
+
+    // Внутренний класс
+    class InnerClass {
+    }
 }
