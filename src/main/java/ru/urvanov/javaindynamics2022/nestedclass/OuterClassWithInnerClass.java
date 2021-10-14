aa0cd04665a610fc22ce5cbece868badaf1fd541
Fedor Urvanov <fedor@urvanov.ru>
2021-10-14 17:47:35 +0300

    OuterClassWithInnerClass

---
+++ src/main/java/ru/urvanov/javaindynamics2022/nestedclass/OuterClassWithInnerClass.java
@@ -0,0 +1,44 @@
+package ru.urvanov.javaindynamics2022.nestedclass;
+
+// Класс с внутренним классом
+class OuterClassWithInnerClass {
+
+
+    private int x1;
+
+    private void method1() {}
+
+    private static void method2() {}
+
+    // Внутренний класс
+    class InnerClass {
+
+        // Внутренние классы имеют доступ
+        // ко всем членам внешнего класса
+        // независимо от модификатора доступа.
+        int y1 = x1;
+
+        {
+            method1();
+            method2();
+        }
+        // ... и т. д.
+
+
+        // Можно обявлять константы
+        public static final int MY_CONSTANT = 34;
+
+        // Никаких других статических членов объявлять
+        // во внутренних классах нельзя!
+        // Будет ошибка компиляции.
+
+        // НЕЛЬЗЯ!
+        //static {
+        //
+        //}
+        //
+
+        // НЕЛЬЗЯ
+        //static void method1() {};
+    }
+}
