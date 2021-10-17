24d05865a533933244443b4537d43d9d4a5d3bdb
Fedor Urvanov <fedor@urvanov.ru>
2021-10-17 16:59:15 +0300

    GreekMonster

--- src/main/java/ru/urvanov/javaindynamics2022/interfaces/GreekMonster.java
+++ src/main/java/ru/urvanov/javaindynamics2022/interfaces/GreekMonster.java
@@ -3,4 +3,9 @@ package ru.urvanov.javaindynamics2022.interfaces;
 public interface GreekMonster {
     boolean isSensitiveToSilver();
     void logic(VisibleWorld visibleWorld);
+
+    // Новый метод
+    default void doSomething() {
+        // Некий код
+    }
 }
