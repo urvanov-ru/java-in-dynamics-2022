7ca382a2fb747e62c034cc8b1ff27cc09524dd0c
Fedor Urvanov <fedor@urvanov.ru>
2021-10-21 11:38:59 +0300

    TypeConversion

---
+++ src/main/java/ru/urvanov/javaindynamics2022/inheritance/TypeConversion.java
@@ -0,0 +1,10 @@
+package ru.urvanov.javaindynamics2022.inheritance;
+
+public class TypeConversion {
+    public static void main(String[] args) {
+        Goblin obj = new Goblin();
+        Object obj1 = new Goblin();
+        Monster obj2 = new Goblin();
+        Goblin goblin = (Goblin) obj;
+    }
+}
