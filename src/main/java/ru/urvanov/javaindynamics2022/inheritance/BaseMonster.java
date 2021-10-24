2106d325d0c88b5bdc309335a64c19a9302abcc0
Fedor Urvanov <fedor@urvanov.ru>
2021-10-24 12:14:54 +0300

    BaseMonster

--- src/main/java/ru/urvanov/javaindynamics2022/inheritance/BaseMonster.java
+++ src/main/java/ru/urvanov/javaindynamics2022/inheritance/BaseMonster.java
@@ -1,5 +1,6 @@
 package ru.urvanov.javaindynamics2022.inheritance;
 
+// Пример абстрактного класса
 abstract class BaseMonster {
 
     abstract void myAbstractMethod(int myParam1, double myParam2);
