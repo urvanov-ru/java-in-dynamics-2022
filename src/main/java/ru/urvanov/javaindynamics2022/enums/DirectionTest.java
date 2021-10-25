8034ce81655e5413c79c50a9a1d2a41e594b0a72
Fedor Urvanov <fedor@urvanov.ru>
2021-10-25 21:14:33 +0300

    DirectionTest

---
+++ src/main/java/ru/urvanov/javaindynamics2022/enums/DirectionTest.java
@@ -0,0 +1,8 @@
+package ru.urvanov.javaindynamics2022.enums;
+
+public class DirectionTest {
+    public static void main(String[] args) {
+        // Объявление переменной
+        Direction direction = Direction.NORTH;
+    }
+}
