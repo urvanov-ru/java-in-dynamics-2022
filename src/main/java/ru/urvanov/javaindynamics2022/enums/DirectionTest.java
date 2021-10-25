b5342fc4e87c52319828d9cce6c9087f9b0a18ee
Fedor Urvanov <fedor@urvanov.ru>
2021-10-25 21:26:56 +0300

    Перебор всех значений перечисления

--- src/main/java/ru/urvanov/javaindynamics2022/enums/DirectionTest.java
+++ src/main/java/ru/urvanov/javaindynamics2022/enums/DirectionTest.java
@@ -4,5 +4,12 @@ public class DirectionTest {
     public static void main(String[] args) {
         // Объявление переменной
         Direction direction = Direction.NORTH;
+
+        // Перебор всех значений перечисления
+        for (Direction dir : Direction.values()) {
+            System.out.println("toString(): " + dir.toString());
+            System.out.println("ordinal(): " + dir.ordinal());
+            System.out.println("name() :" + dir.name());
+        }
     }
 }
