ceaf89a297810bed970edd397d71438db1fcaa40
Fedor Urvanov <fedor@urvanov.ru>
2021-11-21 14:32:55 +0300

    Autoboxing1

---
+++ src/main/java/ru/urvanov/javaindynamics2022/autoboxing/Autoboxing1.java
@@ -0,0 +1,9 @@
+package ru.urvanov.javaindynamics2022.autoboxing;
+
+public class Autoboxing1 {
+    public static void main(String[] args) {
+        Character ch = 'a';
+        Integer i1 = 220;
+        Double d1 = 300.0;
+        Boolean b1 = false;}
+}
