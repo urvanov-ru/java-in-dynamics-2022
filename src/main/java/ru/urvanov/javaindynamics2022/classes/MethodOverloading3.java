c96f213a5fa7663d3490d0eec90be8932efc3166
Fedor Urvanov <fedor@urvanov.ru>
2021-11-23 13:25:55 +0300

    Пример перегрузки методов 3

---
+++ src/main/java/ru/urvanov/javaindynamics2022/classes/MethodOverloading3.java
@@ -0,0 +1,24 @@
+package ru.urvanov.javaindynamics2022.classes;
+
+class MethodOverloading3 {
+
+    static class Monster {}
+
+    static class Goblin extends Monster {}
+
+    static class Hobgoblin extends Goblin {}
+
+    static void method1(Monster obj) {
+        System.out.println("1");
+    }
+
+    static void method1(Goblin obj) {
+        System.out.println("2");
+    }
+
+    public static void main(String[] args) {
+        Monster obj = new Hobgoblin();
+        method1(obj);
+    }
+
+}
