828c7a673c3815f5d3086b51a061b6bc1af6071e
Fedor Urvanov <fedor@urvanov.ru>
2021-11-28 10:41:16 +0300

    Calculator Operation

--- calculator/src/main/java/module-info.java
+++ calculator/src/main/java/module-info.java
@@ -1,2 +1,3 @@
 module ru.urvanov.javaindynamics2022.calculator {
+    exports ru.urvanov.javaindynamics2022.calculator.plugin;
 }
---
+++ calculator/src/main/java/ru/urvanov/javaindynamics2022/calculator/plugin/Operation.java
@@ -0,0 +1,5 @@
+package ru.urvanov.javaindynamics2022.calculator.plugin;
+
+public interface Operation {
+    double calculate(double x, double y);
+}
