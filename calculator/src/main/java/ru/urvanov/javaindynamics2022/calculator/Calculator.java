f2697131436ea4c9a34c90209eed55fe62ef0780
Fedor Urvanov <fedor@urvanov.ru>
2021-11-28 11:52:00 +0300

    Calculator main class

---
+++ calculator/src/main/java/ru/urvanov/javaindynamics2022/calculator/Calculator.java
@@ -0,0 +1,19 @@
+package ru.urvanov.javaindynamics2022.calculator;
+
+import ru.urvanov.javaindynamics2022.calculator.plugin.Operation;
+
+import java.util.ServiceLoader;
+import java.util.stream.StreamSupport;
+
+public class Calculator {
+    public static void main(String[] args) {
+        double x = 100.1;
+        double y = 23.73;
+        ServiceLoader<Operation> sl = ServiceLoader.load(Operation.class);
+        System.out.println("before stream " + sl);
+        StreamSupport.stream(sl.spliterator(), false).forEach(op -> {
+            System.out.println("Operation: " + op.getClass().getName()
+                    + ". Operands: " + x + ", " + y + ". Result = " + op.calculate(x, y));
+        });
+    }
+}
