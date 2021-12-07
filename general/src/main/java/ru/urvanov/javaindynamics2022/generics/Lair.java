ba119eb36c69a0ebc5ce134be2d7b622c6cc0e4a
Fedor Urvanov <fedor@urvanov.ru>
2021-12-07 08:54:59 +0300

    Примем использования обобщённого типа

--- general/src/main/java/ru/urvanov/javaindynamics2022/generics/Lair.java
+++ general/src/main/java/ru/urvanov/javaindynamics2022/generics/Lair.java
@@ -13,4 +13,12 @@ class Lair<T> {
     public T getInhabitant() {
         return this.inhabitant;
     }
+
+    public static void main(String[] args) {
+        Lair<Goblin> goblinLair = new Lair<Goblin>();
+        // указываем жителя.
+        goblinLair.setInhabitant(new Goblin());
+        // Приведение типа уже не нужно.
+        Goblin goblin = goblinLair.getInhabitant();
+    }
 }
