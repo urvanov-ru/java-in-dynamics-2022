72d519ce5a72dffe4fc7a62e6cee287f9ef16cdc
Fedor Urvanov <fedor@urvanov.ru>
2021-12-09 02:12:11 +0300

    FoxSpirit

--- general/src/main/java/ru/urvanov/javaindynamics2022/generics/Utils.java
+++ general/src/main/java/ru/urvanov/javaindynamics2022/generics/Utils.java
@@ -9,10 +9,32 @@ class Utils {
         }
     }
 
+    // Ограниченный обобщённый метод
+    public static<T extends Monster & Enemy & Dreadful> void doSomething(T[] monsters) {
+        T result = null;
+        for (T obj : monsters) {
+
+            obj.doSomething();
+        }
+    }
+
+    static class FoxSpirit  extends Monster implements Enemy, Dreadful {
+
+    }
+
     public static void main(String[] args) {
+        // Вызов обобщённого метода
         Lair<Goblin> goblinsLair = new Lair<>();
         Utils.<Goblin>setIfNull(goblinsLair, new Goblin());
 
         Utils.setIfNull(goblinsLair, new Goblin());
+
+
+        // Вызов ограниченного обобщённого метода.
+        FoxSpirit[] foxSpirits = new FoxSpirit[10];
+        for (int n = 0; n < foxSpirits.length; n++) {
+            foxSpirits[n] = new FoxSpirit();
+        }
+        Utils.doSomething(foxSpirits);
     }
 }
