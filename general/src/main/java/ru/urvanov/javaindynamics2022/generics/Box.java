7780ebc26bbde58946f59ebfe4944eca43d1f831
Fedor Urvanov <fedor@urvanov.ru>
2021-12-09 02:31:00 +0300

    Box

---
+++ general/src/main/java/ru/urvanov/javaindynamics2022/generics/Box.java
@@ -0,0 +1,13 @@
+package ru.urvanov.javaindynamics2022.generics;
+
+public class Box<T> {
+
+    public void add(T value) {
+        // добавление value в Box
+    }
+    public static void main(String[] args) {
+        Box<Number> box = new Box<Number>();
+        box.add(Integer.valueOf(10));   // OK
+        box.add(Double.valueOf(10.1));  // OK
+    }
+}
