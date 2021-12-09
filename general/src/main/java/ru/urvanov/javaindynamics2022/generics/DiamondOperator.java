4d777190ee023986336ed85e4cbd1382f26b43ec
Fedor Urvanov <fedor@urvanov.ru>
2021-12-09 17:42:27 +0300

    DiamondOperator

---
+++ general/src/main/java/ru/urvanov/javaindynamics2022/generics/DiamondOperator.java
@@ -0,0 +1,16 @@
+package ru.urvanov.javaindynamics2022.generics;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+// Выведение типов и создание экземпляра обобщённого класса
+public class DiamondOperator {
+    public static void main(String[] args) {
+        Map<String, List<String>> myMap1 = new HashMap<String, List<String>>();
+
+        Map<String, List<String>> myMap2 = new HashMap<>();
+
+        Map<String, List<String>> myMap3 = new HashMap(); // unchecked conversion warning
+    }
+}
