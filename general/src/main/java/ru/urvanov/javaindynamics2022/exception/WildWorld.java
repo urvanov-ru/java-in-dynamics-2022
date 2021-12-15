bad42be962e240c4be8419d62df8ee78c3dc9afb
Fedor Urvanov <fedor@urvanov.ru>
2021-12-15 16:23:59 +0300

    WildWorld.java

---
+++ general/src/main/java/ru/urvanov/javaindynamics2022/exception/WildWorld.java
@@ -0,0 +1,8 @@
+package ru.urvanov.javaindynamics2022.exception;
+
+class WildWorld {
+    public void someCalculation(int arg1, double arg2)
+            throws java.io.IOException, java.sql.SQLException,
+            java.lang.IndexOutOfBoundsException {
+    }
+}
