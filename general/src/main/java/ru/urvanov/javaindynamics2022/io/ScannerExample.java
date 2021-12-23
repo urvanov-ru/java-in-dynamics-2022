0ab6133a50fbd3368d1044fddc65b9b9e7a08511
Fedor Urvanov <fedor@urvanov.ru>
2021-12-23 15:07:10 +0300

    Scanner Example

---
+++ general/src/main/java/ru/urvanov/javaindynamics2022/io/ScannerExample.java
@@ -0,0 +1,22 @@
+package ru.urvanov.javaindynamics2022.io;
+
+import java.util.Scanner;
+
+/**
+ * Пример использования Scanner
+ */
+public class ScannerExample {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+        double d = sc.nextDouble();
+        // System.out - это PrintStream
+        // Мы используем метод format, в качестве первого
+        // аргумента передаётся строка, содержащая специальные
+        // символы форматирования.
+        // %d - вывод десятичного целого числа
+        // %f - вывод числа с плавающей точкой. Мы дополнительно указываем,
+        // что нужно вывести 10 символов до запятой и 2 символа после запятой
+        System.out.format("i = %d, d = %10.2f", i, d);
+    }
+}
