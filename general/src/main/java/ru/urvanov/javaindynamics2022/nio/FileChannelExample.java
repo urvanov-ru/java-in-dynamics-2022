bb5a9549243c46b2eb55c53e2bd0208b7fceb248
Fedor Urvanov <fedor@urvanov.ru>
2021-12-28 11:03:27 +0300

    FileChannelExample

---
+++ general/src/main/java/ru/urvanov/javaindynamics2022/nio/FileChannelExample.java
@@ -0,0 +1,47 @@
+package ru.urvanov.javaindynamics2022.nio;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.file.Paths;
+
+import static java.nio.file.StandardOpenOption.READ;
+import static java.nio.file.StandardOpenOption.WRITE;
+
+public class FileChannelExample {
+    public static void main(String[] args) {
+        String s = "I was here!\n";
+        byte data[] = s.getBytes();
+        ByteBuffer out = ByteBuffer.wrap(data);
+
+        ByteBuffer copy = ByteBuffer.allocate(12);
+
+        // Вместо output.txt укажите имя существующего файла (его содержимое будет изменено)
+        try (FileChannel fc = (FileChannel.open(Paths.get("output.txt"), READ, WRITE))) {
+            // Читаем первые 12
+            // байт из файла.
+            int nread;
+            do {
+                nread = fc.read(copy);
+            } while (nread != -1 && copy.hasRemaining());
+
+            // Пишем "I was here!" в начало файла.
+            fc.position(0);
+            while (out.hasRemaining())
+                fc.write(out);
+            out.rewind();
+
+            // Перемещаемся в конец файла. Копируем первые 12 байт в
+            // конец файла.  Пишем "I was here!" снова.
+            long length = fc.size();
+            fc.position(length-1);
+            copy.flip();
+            while (copy.hasRemaining())
+                fc.write(copy);
+            while (out.hasRemaining())
+                fc.write(out);
+        } catch (IOException x) {
+            System.out.println("I/O Exception: " + x);
+        }
+    }
+}
