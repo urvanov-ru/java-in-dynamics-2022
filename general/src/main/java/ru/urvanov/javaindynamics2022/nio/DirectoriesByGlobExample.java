afd13c1d9d31ea178b8d5fb491a75b73ed1a6dbf
Fedor Urvanov <fedor@urvanov.ru>
2021-12-28 11:11:09 +0300

    DirectoriesByGlobExample

--- general/src/main/java/ru/urvanov/javaindynamics2022/nio/DirectoriesByGlobExample.java
+++ general/src/main/java/ru/urvanov/javaindynamics2022/nio/DirectoriesByGlobExample.java
@@ -6,7 +6,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-public class DirectoriesByFilterExample {
+public class DirectoriesByGlobExample {
     public static void main(String[] args) {
         // Подставьте сюда путь к каталогу вместо target/classes
         Path dir = Paths.get("target/classes");
