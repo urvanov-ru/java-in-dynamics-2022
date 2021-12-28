083f882fbbcc5a887152bcce61c38903aa843614
Fedor Urvanov <fedor@urvanov.ru>
2021-12-28 11:08:39 +0300

    DirectoriesByFilterExample

---
+++ general/src/main/java/ru/urvanov/javaindynamics2022/nio/DirectoriesByFilterExample.java
@@ -0,0 +1,25 @@
+package ru.urvanov.javaindynamics2022.nio;
+
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class DirectoriesByFilterExample {
+    public static void main(String[] args) {
+        // Подставьте сюда путь к каталогу вместо target/classes
+        Path dir = Paths.get("target/classes");
+        try (DirectoryStream<Path> stream =
+                     Files.newDirectoryStream(dir, "*.{java,class,jar}")) {
+            for (Path entry: stream) {
+                System.out.println(entry.getFileName());
+            }
+        } catch (IOException x) {
+            // IOException никогда не бросится во время итерации.
+            // В этом куске кода оно может броситься только
+            // методом newDirectoryStream
+            System.err.println(x);
+        }
+    }
+}
