83b549992b68e0468c39459da191e2a54a6d2de9
Fedor Urvanov <fedor@urvanov.ru>
2022-01-01 15:54:31 +0300

    HelloRunnable

---
+++ general/src/main/java/ru/urvanov/javaindynamics2022/multithreading/HelloRunnable.java
@@ -0,0 +1,13 @@
+package ru.urvanov.javaindynamics2022.multithreading;
+
+public class HelloRunnable implements Runnable {
+
+    public void run() {
+        System.out.println("Hello from a thread!");
+    }
+
+    public static void main(String args[]) {
+        (new Thread(new HelloRunnable())).start();
+    }
+
+}
