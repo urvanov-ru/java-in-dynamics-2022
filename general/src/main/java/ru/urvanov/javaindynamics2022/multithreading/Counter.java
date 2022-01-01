26a3899ef54c18abccaa7636bd94ef4c66b90153
Fedor Urvanov <fedor@urvanov.ru>
2022-01-01 16:25:15 +0300

    Counter

---
+++ general/src/main/java/ru/urvanov/javaindynamics2022/multithreading/Counter.java
@@ -0,0 +1,18 @@
+package ru.urvanov.javaindynamics2022.multithreading;
+
+class Counter {
+    private int c = 0;
+
+    public void increment() {
+        c++;
+    }
+
+    public void decrement() {
+        c--;
+    }
+
+    public int value() {
+        return c;
+    }
+
+}
