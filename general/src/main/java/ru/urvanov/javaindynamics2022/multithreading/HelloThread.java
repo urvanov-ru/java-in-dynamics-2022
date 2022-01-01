ff8f208299ddf49540069b94fc5833e5d92d08e1
Fedor Urvanov <fedor@urvanov.ru>
2022-01-01 15:58:23 +0300

    HelloThread

---
+++ general/src/main/java/ru/urvanov/javaindynamics2022/multithreading/HelloThread.java
@@ -0,0 +1,13 @@
+package ru.urvanov.javaindynamics2022.multithreading;
+
+public class HelloThread extends Thread {
+
+    public void run() {
+        System.out.println("Hello from a thread!");
+    }
+
+    public static void main(String args[]) {
+        (new HelloThread()).start();
+    }
+
+}
