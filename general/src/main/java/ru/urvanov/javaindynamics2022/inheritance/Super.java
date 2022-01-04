1f54a3e6a35b183f52c78a74e08e8f457837c47b
Fedor Urvanov <fedor@urvanov.ru>
2022-01-04 13:56:27 +0300

    Комментарий

--- general/src/main/java/ru/urvanov/javaindynamics2022/inheritance/Super.java
+++ general/src/main/java/ru/urvanov/javaindynamics2022/inheritance/Super.java
@@ -1,7 +1,7 @@
 package ru.urvanov.javaindynamics2022.inheritance;
 
 /**
- * Пример для ключевого слова super
+ * Пример с ключевым словом super
  */
 public class Super {
     public static void main(String[] args) {
