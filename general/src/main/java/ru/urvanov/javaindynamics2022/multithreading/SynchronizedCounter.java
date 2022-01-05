f60255cd090782542f56910294d4e3eb4f13e8a6
Fedor Urvanov <fedor@urvanov.ru>
2022-01-05 17:52:06 +0300

    SynchronizedCounter

---
+++ general/src/main/java/ru/urvanov/javaindynamics2022/multithreading/SynchronizedCounter.java
@@ -0,0 +1,17 @@
+package ru.urvanov.javaindynamics2022.multithreading;
+
+public class SynchronizedCounter {
+    private int c = 0;
+
+    public synchronized void increment() {
+        c++;
+    }
+
+    public synchronized void decrement() {
+        c--;
+    }
+
+    public synchronized int value() {
+        return c;
+    }
+}
