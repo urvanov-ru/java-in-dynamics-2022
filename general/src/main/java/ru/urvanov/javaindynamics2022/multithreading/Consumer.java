659d106089caf20aeb188b0e13f8b4f3f3de3d0d
Fedor Urvanov <fedor@urvanov.ru>
2022-01-05 18:28:08 +0300

    ProducerConsumerExample

---
+++ general/src/main/java/ru/urvanov/javaindynamics2022/multithreading/Consumer.java
@@ -0,0 +1,23 @@
+package ru.urvanov.javaindynamics2022.multithreading;
+
+import java.util.Random;
+
+public class Consumer implements Runnable {
+    private Drop drop;
+
+    public Consumer(Drop drop) {
+        this.drop = drop;
+    }
+
+    public void run() {
+        Random random = new Random();
+        for (String message = drop.take();
+             ! message.equals("DONE");
+             message = drop.take()) {
+            System.out.format("MESSAGE RECEIVED: %s%n", message);
+            try {
+                Thread.sleep(random.nextInt(5000));
+            } catch (InterruptedException e) {}
+        }
+    }
+}
---
+++ general/src/main/java/ru/urvanov/javaindynamics2022/multithreading/ProducerConsumerExample.java
@@ -0,0 +1,9 @@
+package ru.urvanov.javaindynamics2022.multithreading;
+
+public class ProducerConsumerExample {
+    public static void main(String[] args) {
+        Drop drop = new Drop();
+        (new Thread(new Producer(drop))).start();
+        (new Thread(new Consumer(drop))).start();
+    }
+}
