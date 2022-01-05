ad2dd209739f290f4683b6c3ee1fa8a29a2be98d
Fedor Urvanov <fedor@urvanov.ru>
2022-01-05 18:23:24 +0300

    Producer

---
+++ general/src/main/java/ru/urvanov/javaindynamics2022/multithreading/Producer.java
@@ -0,0 +1,31 @@
+package ru.urvanov.javaindynamics2022.multithreading;
+
+import java.util.Random;
+
+public class Producer implements Runnable {
+    private Drop drop;
+
+    public Producer(Drop drop) {
+        this.drop = drop;
+    }
+
+    public void run() {
+        String importantInfo[] = {
+                "Mares eat oats",
+                "Does eat oats",
+                "Little lambs eat ivy",
+                "A kid will eat ivy too"
+        };
+        Random random = new Random();
+
+        for (int i = 0;
+             i < importantInfo.length;
+             i++) {
+            drop.put(importantInfo[i]);
+            try {
+                Thread.sleep(random.nextInt(5000));
+            } catch (InterruptedException e) {}
+        }
+        drop.put("DONE");
+    }
+}
