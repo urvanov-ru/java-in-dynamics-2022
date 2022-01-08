8f09e15732a1bade6985235aee668201862e4f1f
Fedor Urvanov <fedor@urvanov.ru>
2022-01-08 17:42:13 +0300

    SimpleCompletableFuture example

--- general/src/main/java/ru/urvanov/javaindynamics2022/multithreading/SimpleCompletableFuture.java
+++ general/src/main/java/ru/urvanov/javaindynamics2022/multithreading/SimpleCompletableFuture.java
@@ -6,10 +6,10 @@ import java.util.concurrent.Executors;
 
 public class SimpleCompletableFuture {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        CompletableFuture<String> future1 = CompletableFuture.supplyAsync(
-                () -> "Test1");
-        CompletableFuture<String> future2 = CompletableFuture.supplyAsync(
-                () -> "Test2", Executors.newCachedThreadPool());
+        CompletableFuture<String> future1 = CompletableFuture
+                .supplyAsync(() -> "Test1");
+        CompletableFuture<String> future2 = CompletableFuture
+                .supplyAsync(() -> "Test2", Executors.newCachedThreadPool());
         System.out.println(future1.get());
         System.out.println(future2.get());
 
@@ -18,5 +18,26 @@ public class SimpleCompletableFuture {
         CompletableFuture<Void> future4 = CompletableFuture.runAsync(
                 () -> System.out.println("Test4"),
                 Executors.newCachedThreadPool());
+
+
+
+        // thenAccept
+        CompletableFuture<String> completableFutureThenAccept
+                = CompletableFuture.supplyAsync(() -> "Hello");
+
+        CompletableFuture<Void> futurethenAccept = completableFutureThenAccept
+                .thenAccept(s -> System.out.println("Computation returned: " + s));
+
+        futurethenAccept.get();
+
+
+        // thenRun
+        CompletableFuture<String> completableFutureThenRun
+                = CompletableFuture.supplyAsync(() -> "Hello");
+
+        CompletableFuture<Void> futureThenRun = completableFutureThenRun
+                .thenRun(() -> System.out.println("Computation finished."));
+
+        futureThenRun.get();
     }
 }
