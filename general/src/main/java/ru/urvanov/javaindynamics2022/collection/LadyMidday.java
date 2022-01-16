06b98958fc7a2175928b4646ffc75ee0e5ee727f
Fedor Urvanov <fedor@urvanov.ru>
2022-01-16 11:32:00 +0300

    StudentScore and StudentSort

---
+++ general/src/main/java/ru/urvanov/javaindynamics2022/collection/LadyMidday.java
@@ -0,0 +1,48 @@
+package ru.urvanov.javaindynamics2022.collection;
+
+public class LadyMidday implements Comparable<LadyMidday> {
+    private String name;
+    private int level;
+    private int money;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
+
+    @Override
+    public int compareTo(LadyMidday other) {
+        if (other == null) {
+            throw new NullPointerException();
+        }
+        return Integer.compare(level, other.getLevel());
+    }
+
+    @Override
+    public String toString() {
+        return "LadyMidday{" +
+                "name='" + name + '\'' +
+                ", level=" + level +
+                ", money=" + money +
+                '}';
+    }
+}
