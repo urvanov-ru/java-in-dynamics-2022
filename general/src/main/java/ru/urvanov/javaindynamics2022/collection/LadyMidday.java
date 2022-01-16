95549d7af1438a8faa8c1b86063067b8fafd390b
Fedor Urvanov <fedor@urvanov.ru>
2022-01-16 11:40:36 +0300

    LadyMidday

--- general/src/main/java/ru/urvanov/javaindynamics2022/collection/LadyMidday.java
+++ general/src/main/java/ru/urvanov/javaindynamics2022/collection/LadyMidday.java
@@ -5,6 +5,12 @@ public class LadyMidday implements Comparable<LadyMidday> {
     private int level;
     private int money;
 
+    public LadyMidday(String name, int level, int money) {
+        this.name = name;
+        this.level = level;
+        this.money = money;
+    }
+
     public String getName() {
         return name;
     }
