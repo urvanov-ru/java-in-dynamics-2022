a5ed2a5416ab0da1c58476c07d10336413b39493
Fedor Urvanov <fedor@urvanov.ru>
2022-01-19 15:03:20 +0300

    Hobgoblin

--- general/src/main/java/ru/urvanov/javaindynamics2022/collection/Hobgoblin.java
+++ general/src/main/java/ru/urvanov/javaindynamics2022/collection/Hobgoblin.java
@@ -1,18 +1,21 @@
 package ru.urvanov.javaindynamics2022.collection;
 
-import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Hobgoblin {
-    public enum Gender {
-        MALE, FEMALE
-    }
 
-    String name;
-    LocalDate birthdate;
-    Gender gender;
-    int gold;
-    double health = 100.0;
-    double power = 30.0;
+    private String name;
+    private int gold;
+    private double health = 100.0;
+    private double power = 30.0;
+
+    public Hobgoblin(String name, int gold, double health, double power) {
+        this.name = name;
+        this.gold = gold;
+        this.health = health;
+        this.power = power;
+    }
 
     public String getName() {
         return name;
@@ -20,22 +23,6 @@ public class Hobgoblin {
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public LocalDate getBirthdate() {
-        return birthdate;
-    }
-
-    public void setBirthdate(LocalDate birthdate) {
-        this.birthdate = birthdate;
-    }
-
-    public Gender getGender() {
-        return gender;
-    }
-
-    public void setGender(Gender gender) {
-        this.gender = gender;
     }
 
     public int getGold() {
@@ -66,11 +53,33 @@ public class Hobgoblin {
     public String toString() {
         return "Hobgoblin{" +
                 "name='" + name + '\'' +
-                ", birthdate=" + birthdate +
-                ", gender=" + gender +
                 ", gold=" + gold +
                 ", health=" + health +
                 ", power=" + power +
                 '}';
+    }
+
+    public static void main(String[] args) {
+        List<Hobgoblin> hobgoblins = new ArrayList<>();
+        hobgoblins.add(new Hobgoblin("Ужасный Фро", 2, 99, 31));
+        hobgoblins.add(new Hobgoblin("Стареющий Эд", 23, 36, 12));
+        hobgoblins.add(new Hobgoblin("Богатейший Джолливер", 300, 80, 25));
+        hobgoblins.add(new Hobgoblin("Дохляк Эрни", 6, 88, 15));
+        hobgoblins.add(new Hobgoblin("Бледный Сэм", 59, 60, 15));
+
+        System.out.println("Список домовых:");
+        for (Hobgoblin hobgoblin : hobgoblins) {
+            System.out.println(hobgoblin.getName());
+        }
+
+        System.out.println();
+
+        System.out.println("Список домовых с помощью Java Stream API:");
+        hobgoblins
+                .stream()
+                .map(Hobgoblin::getName)
+                .forEach(System.out::println);
+
+        System.out.println();
     }
 }
