cdc6f4a38c891c836691b091a8403f232ae72d4b
Fedor Urvanov <fedor@urvanov.ru>
2022-01-19 14:43:58 +0300

    Hobgoblin

---
+++ general/src/main/java/ru/urvanov/javaindynamics2022/collection/Hobgoblin.java
@@ -0,0 +1,76 @@
+package ru.urvanov.javaindynamics2022.collection;
+
+import java.time.LocalDate;
+
+public class Hobgoblin {
+    public enum Sex {
+        MALE, FEMALE
+    }
+
+    String name;
+    LocalDate birthdate;
+    Sex gender;
+    int gold;
+    double health = 100.0;
+    double power = 30.0;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(LocalDate birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public Sex getGender() {
+        return gender;
+    }
+
+    public void setGender(Sex gender) {
+        this.gender = gender;
+    }
+
+    public int getGold() {
+        return gold;
+    }
+
+    public void setGold(int gold) {
+        this.gold = gold;
+    }
+
+    public double getHealth() {
+        return health;
+    }
+
+    public void setHealth(double health) {
+        this.health = health;
+    }
+
+    public double getPower() {
+        return power;
+    }
+
+    public void setPower(double power) {
+        this.power = power;
+    }
+
+    @Override
+    public String toString() {
+        return "Hobgoblin{" +
+                "name='" + name + '\'' +
+                ", birthdate=" + birthdate +
+                ", gender=" + gender +
+                ", gold=" + gold +
+                ", health=" + health +
+                ", power=" + power +
+                '}';
+    }
+}
