b4a85942f81a5b18f7660e126bc08d769a2eb838
Fedor Urvanov <fedor@urvanov.ru>
2022-01-28 10:11:27 +0300

    Year

---
+++ general/src/main/java/ru/urvanov/javaindynamics2022/datetime/YearExample.java
@@ -0,0 +1,10 @@
+package ru.urvanov.javaindynamics2022.datetime;
+
+import java.time.Year;
+
+public class YearExample {
+    public static void main(String[] args) {
+        Year year = Year.of(2022);
+        System.out.println(year);
+    }
+}
