a9eea3c0f14569c6001dc8cbf7117083a2a226c3
Fedor Urvanov <fedor@urvanov.ru>
2022-01-28 10:21:58 +0300

    Year

---
+++ general/src/main/java/ru/urvanov/javaindynamics2022/datetime/LocalTimeExample.java
@@ -0,0 +1,16 @@
+package ru.urvanov.javaindynamics2022.datetime;
+
+import java.time.LocalTime;
+
+public class LocalTimeExample {
+    public static void main(String[] args) {
+        // 10 часов 30 минут
+        LocalTime localTime0 = LocalTime.of(10, 30);
+
+        // 10 часов 30 минут 45 секунд
+        LocalTime localTime1 = LocalTime.of(10, 30, 45);
+
+        // 10 часов 30 минут 45 секунд 100 наносекунд
+        LocalTime localTime2 = LocalTime.of(10, 30, 45, 100);
+    }
+}
