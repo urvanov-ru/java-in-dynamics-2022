24ea9e6ddb4c8986f6434d61665dd90061cc30a0
Fedor Urvanov <fedor@urvanov.ru>
2022-01-28 09:57:55 +0300

    YearMonth

---
+++ general/src/main/java/ru/urvanov/javaindynamics2022/datetime/YearMonthExample.java
@@ -0,0 +1,17 @@
+package ru.urvanov.javaindynamics2022.datetime;
+
+import java.time.Month;
+import java.time.YearMonth;
+
+public class YearMonthExample {
+    public static void main(String[] args) {
+        YearMonth date = YearMonth.now();
+        System.out.printf("%s: %d%n", date, date.lengthOfMonth());
+
+        YearMonth date2 = YearMonth.of(2010, Month.FEBRUARY);
+        System.out.printf("%s: %d%n", date2, date2.lengthOfMonth());
+
+        YearMonth date3 = YearMonth.of(2012, Month.FEBRUARY);
+        System.out.printf("%s: %d%n", date3, date3.lengthOfMonth());
+    }
+}
