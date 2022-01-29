2c9fea211870c32983a26217a3c6a28494f9f295
Fedor Urvanov <fedor@urvanov.ru>
2022-01-30 01:24:22 +0300

    LocalDate plus minus

--- general/src/main/java/ru/urvanov/javaindynamics2022/datetime/LocalDateExample.java
+++ general/src/main/java/ru/urvanov/javaindynamics2022/datetime/LocalDateExample.java
@@ -7,5 +7,11 @@ public class LocalDateExample {
     public static void main(String[] args) {
         LocalDate date = LocalDate.of(2022, Month.NOVEMBER, 20);
         System.out.println(date);
+
+        LocalDate date2 = date.plusYears(1);
+        System.out.println(date2);
+
+        LocalDate date3 = date.minusDays(3);
+        System.out.println(date3);
     }
 }
