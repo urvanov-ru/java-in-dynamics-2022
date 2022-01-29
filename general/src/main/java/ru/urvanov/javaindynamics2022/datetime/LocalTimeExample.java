798bc509d9450cf49382f1dbe483cde48db73f0b
Fedor Urvanov <fedor@urvanov.ru>
2022-01-30 01:38:04 +0300

    LocalTime plus minus

--- general/src/main/java/ru/urvanov/javaindynamics2022/datetime/LocalTimeExample.java
+++ general/src/main/java/ru/urvanov/javaindynamics2022/datetime/LocalTimeExample.java
@@ -12,5 +12,10 @@ public class LocalTimeExample {
 
         // 10 часов 30 минут 45 секунд 100 наносекунд
         LocalTime localTime2 = LocalTime.of(10, 30, 45, 100);
+
+
+        // Методы plus* и minus*
+        LocalTime localTime3 = localTime0.plusHours(2).minusMinutes(10).plusSeconds(15);
+
     }
 }
