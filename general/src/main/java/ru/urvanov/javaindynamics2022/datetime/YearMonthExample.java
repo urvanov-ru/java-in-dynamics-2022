0be15731bf713f308c24b121b1b7c712a3d8a4ca
Fedor Urvanov <fedor@urvanov.ru>
2022-01-30 01:29:10 +0300

    YearMonth plus minus

--- general/src/main/java/ru/urvanov/javaindynamics2022/datetime/YearMonthExample.java
+++ general/src/main/java/ru/urvanov/javaindynamics2022/datetime/YearMonthExample.java
@@ -13,5 +13,8 @@ public class YearMonthExample {
 
         YearMonth date3 = YearMonth.of(2012, Month.FEBRUARY);
         System.out.printf("%s: %d%n", date3, date3.lengthOfMonth());
+
+        YearMonth date4 = date.plusMonths(2).minusYears(3);
+        System.out.println(date4);
     }
 }
