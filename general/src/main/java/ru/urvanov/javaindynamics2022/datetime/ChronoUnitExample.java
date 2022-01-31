0d26ebc1caf03198b0a5e561b2cee783bcd30f7c
Fedor Urvanov <fedor@urvanov.ru>
2022-01-31 18:47:39 +0300

    ChronoUnitExample

---
+++ general/src/main/java/ru/urvanov/javaindynamics2022/datetime/ChronoUnitExample.java
@@ -0,0 +1,17 @@
+package ru.urvanov.javaindynamics2022.datetime;
+
+import java.time.Instant;
+import java.time.temporal.Temporal;
+import java.time.temporal.ChronoUnit;
+
+public class ChronoUnitExample {
+    public static void main(String[] args) {
+
+        Instant previous = Instant.now(), current = Instant.now();
+        long gap;
+        current = Instant.now();
+        if (previous != null) {
+            gap = ChronoUnit.MILLIS.between(previous,current);
+        }
+    }
+}
