bd6e38036bf8f924b35505dca1ac8c2dfbd2e962
Fedor Urvanov <fedor@urvanov.ru>
2022-01-31 09:15:21 +0300

    DateTimeFormatterExample

--- general/src/main/java/ru/urvanov/javaindynamics2022/datetime/DateTimeFormatterExample.java
+++ general/src/main/java/ru/urvanov/javaindynamics2022/datetime/DateTimeFormatterExample.java
@@ -18,24 +18,31 @@ public class DateTimeFormatterExample {
                 0,  // Секунды
                 0, // Наносекунды
                 ZoneId.of("Europe/Moscow"));
-        System.out.println(zonedDateTime.format(DateTimeFormatter.ISO_ZONED_DATE_TIME));
+        String formattedIso = zonedDateTime.format(DateTimeFormatter.ISO_ZONED_DATE_TIME);
+        System.out.println(formattedIso);
 
+        DateTimeFormatter fullFormatter = DateTimeFormatter
+                .ofLocalizedDateTime(FormatStyle.FULL, FormatStyle.FULL)
+                .withLocale(new Locale("ru", "RU"));
+        String formattedFull = zonedDateTime.format(fullFormatter);
+        System.out.println(formattedFull);
 
-        System.out.println(zonedDateTime.format(
-                DateTimeFormatter.ofLocalizedDateTime(FormatStyle.FULL, FormatStyle.FULL)
-                .withLocale(new Locale("ru", "RU"))));
-
-        System.out.println(zonedDateTime.format(
-                DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT, FormatStyle.SHORT)
-                .withLocale(new Locale("ru", "RU"))));
+        DateTimeFormatter shortFormatter = DateTimeFormatter.ofLocalizedDateTime(
+                        FormatStyle.SHORT, FormatStyle.SHORT)
+                .withLocale(new Locale("ru", "RU"));
+        String formattedShort = zonedDateTime.format(shortFormatter);
+        System.out.println(formattedShort);
 
         // С локальню по умолчанию
-        System.out.println(zonedDateTime.format(
-                DateTimeFormatter.ofLocalizedDateTime(FormatStyle.FULL, FormatStyle.FULL)));
+        String formattedWithSystemLocale = zonedDateTime.format(
+                DateTimeFormatter.ofLocalizedDateTime(
+                        FormatStyle.FULL, FormatStyle.FULL));
+        System.out.println(formattedWithSystemLocale);
 
-        TemporalAccessor parsed = DateTimeFormatter.ISO_ZONED_DATE_TIME.parse(
-                "2022-01-31T08:51:00+03:00[Europe/Moscow]");
-        System.out.println(parsed);
+        ZonedDateTime parsedZonedDateTime = ZonedDateTime.parse(
+                "2022-01-31T08:51:00+03:00[Europe/Moscow]",
+                DateTimeFormatter.ISO_ZONED_DATE_TIME);
+        System.out.println(parsedZonedDateTime);
 
     }
 }
