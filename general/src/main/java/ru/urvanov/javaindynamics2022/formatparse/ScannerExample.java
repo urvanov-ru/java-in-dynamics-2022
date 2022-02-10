75edf13d62aac903bf1b03f692036e042d6b2768
Fedor Urvanov <fedor@urvanov.ru>
2022-02-10 10:57:23 +0300

    ScannerExample

---
+++ general/src/main/java/ru/urvanov/javaindynamics2022/formatparse/ScannerExample.java
@@ -0,0 +1,23 @@
+package ru.urvanov.javaindynamics2022.formatparse;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+public class ScannerExample {
+    public static void main(String[] args) throws IOException {
+
+        Scanner s = null;
+
+        try {
+            s = new Scanner(new BufferedReader(new FileReader("scanner-data.txt", StandardCharsets.UTF_8)));
+
+            while (s.hasNext()) {
+                System.out.println(s.next());
+            }
+        } finally {
+            if (s != null) {
+                s.close();
+            }
+        }
+    }
+}
