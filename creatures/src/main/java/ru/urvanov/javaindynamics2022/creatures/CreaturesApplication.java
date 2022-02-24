505dfbdd03531e874c99a9fd7ee22f0b711f5f05
Fedor Urvanov <fedor@urvanov.ru>
2022-02-24 16:38:16 +0300

    Creatures Spring Boot Application

---
+++ creatures/src/main/java/ru/urvanov/javaindynamics2022/creatures/CreaturesApplication.java
@@ -0,0 +1,13 @@
+package ru.urvanov.javaindynamics2022.creatures;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreaturesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreaturesApplication.class, args);
+    }
+
+}
