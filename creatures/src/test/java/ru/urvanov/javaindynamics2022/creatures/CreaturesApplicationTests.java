22ae8b63835fbc55dda7690c7d62bb14c09cf7b7
Fedor Urvanov <fedor@urvanov.ru>
2022-02-24 16:06:05 +0300

    Creatures Spring Boot Application

---
+++ creatures/src/test/java/ru/urvanov/javaindynamics2022/creatures/CreaturesApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.urvanov.javaindynamics2022.creatures;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreaturesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
