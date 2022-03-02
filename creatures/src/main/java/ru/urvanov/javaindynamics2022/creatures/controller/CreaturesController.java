6671df1c8f9e763b5f8be0ad02ac503497b5b22e
Fedor Urvanov <fedor@urvanov.ru>
2022-03-02 10:39:47 +0300

    Creatures Controller

---
+++ creatures/src/main/java/ru/urvanov/javaindynamics2022/creatures/controller/CreaturesController.java
@@ -0,0 +1,9 @@
+package ru.urvanov.javaindynamics2022.creatures.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/creatures")
+public class CreaturesController {
+}
--- creatures/src/main/java/ru/urvanov/javaindynamics2022/creatures/controller/CreaturesController.kt
+++
@@ -1,9 +0,0 @@
-package ru.urvanov.javaindynamics2022.creatures.controller
-
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
-
-@RestController
-@RequestMapping("/creatures")
-class CreaturesController {
-}
