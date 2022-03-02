39a2f771fabe11ed17ca814a32b654c30e5c6514
Fedor Urvanov <fedor@urvanov.ru>
2022-03-02 11:25:20 +0300

    Creatures Controller

--- creatures/src/main/java/ru/urvanov/javaindynamics2022/creatures/controller/CreaturesController.java
+++ creatures/src/main/java/ru/urvanov/javaindynamics2022/creatures/controller/CreaturesController.java
@@ -1,9 +1,32 @@
 package ru.urvanov.javaindynamics2022.creatures.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/creatures")
 public class CreaturesController {
+
+    private Logger logger = LoggerFactory.getLogger(CreaturesController.class);
+
+    @PostMapping
+    public void create() {
+        logger.debug("Создание существа");
+    }
+
+    @PutMapping("{creatureId}")
+    public void update(@PathVariable Long creatureId) {
+        logger.debug("Обновление информации о существе");
+    }
+
+    @GetMapping("{creatureId}")
+    public void info(@PathVariable Long creatureId) {
+        logger.debug("Получение информации о существе");
+    }
+
+    @DeleteMapping("{creatureId}")
+    public void delete(@PathVariable Long creatureId) {
+        logger.debug("Удаление существа");
+    }
 }
