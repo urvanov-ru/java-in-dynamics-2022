e8bd52d88e5daa43a3f2b180bc5cba076c8bdcb4
Fedor Urvanov <fedor@urvanov.ru>
2022-03-21 20:32:38 +0300

    Dockerfile

---
+++ creatures/src/main/java/ru/urvanov/javaindynamics2022/creatures/controller/HomeController.java
@@ -0,0 +1,12 @@
+package ru.urvanov.javaindynamics2022.creatures.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @RequestMapping("/")
+    public String home() {
+        return "Hello World!";
+    }
+}
