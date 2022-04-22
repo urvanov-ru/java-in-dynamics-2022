4810bc7640cedea99668c3b9b874dcb70d6bed66
Fedor Urvanov <fedor@urvanov.ru>
2022-04-22 13:38:59 +0300

    WildcardEngineExample

--- general/src/main/java/ru/urvanov/javaindynamics2022/generics/WildcardEngineExample.java
+++ general/src/main/java/ru/urvanov/javaindynamics2022/generics/WildcardEngineExample.java
@@ -14,4 +14,15 @@ public class WildcardEngineExample {
         // Ошибка компиляции
         // ants.set(0, ants.get(0));
     }
+
+    void processAntsFixed(List<?> ants) {
+        processAntsHelper(ants);
+    }
+
+    // Вспомогательный метод создан так,
+    // чтобы подстановочный символ мог быть захвачен.
+    // через выведение типа.
+    private <T> void processAntsHelper(List<T> ants) {
+        ants.set(0, ants.get(0));
+    }
 }
