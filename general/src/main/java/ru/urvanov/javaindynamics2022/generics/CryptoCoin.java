d02598c3beafc0e190f57875bea94a715a766ce7
Fedor Urvanov <fedor@urvanov.ru>
2022-04-22 13:12:58 +0300

    ElvenCoin, CryptoCoin

--- general/src/main/java/ru/urvanov/javaindynamics2022/generics/CryptoCoin.java
+++ general/src/main/java/ru/urvanov/javaindynamics2022/generics/CryptoCoin.java
@@ -1,9 +1,28 @@
 package ru.urvanov.javaindynamics2022.generics;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CryptoCoin {
     private String name;
 
     public CryptoCoin(String name) {
         this.name = name;
     }
+
+    public static void main(String[] args) {
+        List<ElvenCoin> ec = new ArrayList<>();
+        List<? extends CryptoCoin> cc = ec;
+        // ошибка компиляции
+        // cc.add(new CryptoCoin("DGC"));
+    }
+}
+
+
+class ElvenCoin extends CryptoCoin {
+    public ElvenCoin() {
+        super("ELC");
+    }
+
+
 }
