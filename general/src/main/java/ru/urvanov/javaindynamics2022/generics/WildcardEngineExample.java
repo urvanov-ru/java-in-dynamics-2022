3faa923eb60b8bdc84224f5f9e60684c9e61193e
Fedor Urvanov <fedor@urvanov.ru>
2022-04-22 13:44:21 +0300

    WildcardEngineExample

--- general/src/main/java/ru/urvanov/javaindynamics2022/generics/WildcardEngineExample.java
+++ general/src/main/java/ru/urvanov/javaindynamics2022/generics/WildcardEngineExample.java
@@ -25,4 +25,14 @@ public class WildcardEngineExample {
     private <T> void processAntsHelper(List<T> ants) {
         ants.set(0, ants.get(0));
     }
+
+    void swapAnts(List<? extends Ant> l1, List<? extends Ant> l2) {
+        Ant ant = l1.get(0);
+
+        // Нельзя ошибка компиляции
+        // l1.set(0, l2.get(0));
+
+        // Нельзя ошибка компиляции
+        // l2.set(0, ant);
+    }
 }
