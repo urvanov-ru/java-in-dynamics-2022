8f5e4166e4cbba73911a99e8132514daccfcd4d8
Fedor Urvanov <fedor@urvanov.ru>
2022-05-04 18:44:52 +0300

    FantasyWorldEngine, HeapPollutionExample

--- general/src/main/java/ru/urvanov/javaindynamics2022/generics/FantasyWorldEngine.java
+++ general/src/main/java/ru/urvanov/javaindynamics2022/generics/FantasyWorldEngine.java
@@ -1,5 +1,6 @@
 package ru.urvanov.javaindynamics2022.generics;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -16,6 +17,37 @@ public class FantasyWorldEngine {
         objectArray[0] = Arrays.asList("просто любой объект");
         Nymph s = lists[0].get(0);       // Здесь генерируется
                                           // ClassCastException
+    }
+
+    public static void main(String[] args) {
+
+        List<Nymph> stringListA = new ArrayList<>();
+        List<Nymph> stringListB = new ArrayList<>();
+
+        FantasyWorldEngine.union(stringListA,
+                new Nymph("Эльза"),
+                new Nymph("Мария"),
+                new Nymph("Яна"),
+                new Nymph("Оксана"));
+        FantasyWorldEngine.union(stringListB,
+                new Nymph("Джамиля"),
+                new Nymph("Конни"),
+                new Nymph("Екатерина"),
+                new Nymph("Алиса"));
+        List<List<Nymph>> listOfNymphLists =
+                new ArrayList<>();
+        FantasyWorldEngine.union(listOfNymphLists,
+                stringListA, stringListB);
+
+        // Выйдет ошибка, так как внутри метода вставляем строку
+        // внутрь списка Нимф, а потом при попытке вытащить его оттуда
+        // получаем ошибку.
+        FantasyWorldEngine.invalid(
+                List.of(
+                        new Nymph("Конни"),
+                        new Nymph("Эльвира")),
+                List.of(new Nymph("Джамиля"),
+                        new Nymph("Яна")));
     }
 }
 
