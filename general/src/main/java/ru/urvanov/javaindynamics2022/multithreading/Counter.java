763dece485a1566a101e97d4d51631c880cbca8b
Fedor Urvanov <fedor@urvanov.ru>
2022-05-06 14:51:55 +0300

    Coune

--- general/src/main/java/ru/urvanov/javaindynamics2022/multithreading/Counter.java
+++ general/src/main/java/ru/urvanov/javaindynamics2022/multithreading/Counter.java
@@ -1,18 +1,37 @@
 package ru.urvanov.javaindynamics2022.multithreading;
 
+// Пример вмешательства в поток (thread interference)
 class Counter {
-    private int c = 0;
+    private int value = 0;
 
     public void increment() {
-        c++;
+        for (int n = 0; n < 100_000; n++) {
+            value++;
+        }
     }
 
     public void decrement() {
-        c--;
+        for (int n = 0; n < 100_000; n++) {
+            value--;
+        }
     }
 
-    public int value() {
-        return c;
+    public static void main(String[] args) throws InterruptedException {
+        Counter counter = new Counter();
+        Thread threadIncrement = new Thread(counter::increment);
+        Thread threadDecrement = new Thread(counter::decrement);
+        threadIncrement.start();
+        threadDecrement.start();
+        threadIncrement.join();
+        threadDecrement.join();
+        // Каждый раз будем получать разный результат
+        // из-за того что потоки несинхронизированы.
+        // Это последствия thread interference.
+        System.out.println(counter.getValue());
+    }
+
+    private int getValue() {
+        return value;
     }
 
 }
